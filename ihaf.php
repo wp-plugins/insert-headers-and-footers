7bc2f151449edbd54a0b19b0013c2d4ac96e9100
iamdpegg <iamdpegg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-13 17:27:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/insert-headers-and-footers/trunk@571874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- ihaf.php
+++ ihaf.php
@@ -4,8 +4,8 @@ Plugin Name: Insert Headers and Footers
 Plugin URI: 
 Description: Allows you to insert code or text in the header or footer of your WordPress blog
 Version: 1.0
-Author: David Pegg
-Author URI:
+Author: iamdpegg
+Author URI: 
 License: This software is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
